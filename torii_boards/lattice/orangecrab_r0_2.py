amaranth-lang/amaranth-boards

orangecrab_r0_2: add 85F device variant
Signed-off-by: Daniel Maslowski <info@orangecms.org>

amaranth_boards/orangecrab_r0_2.py
@@ -6,12 +6,12 @@ from amaranth.build import *
 from amaranth.vendor.lattice_ecp5 import *
 from .resources import *
 
+# NOTE: Keep OrangeCrabR0_2FPlatform for backwards compatibility
+# Originally, there was only OrangeCrabR0_2FPlatform, but the 85F variant
+# needs a different device.
+__all__ = ["OrangeCrabR0_2FPlatform", "OrangeCrabR0_2_25FPlatform", "OrangeCrabR0_2_85FPlatform"]
 
-__all__ = ["OrangeCrabR0_2Platform"]
-
-
-class OrangeCrabR0_2Platform(LatticeECP5Platform):
-    device      = "LFE5U-25F"
+class _OrangeCrabR0_2Platform(LatticeECP5Platform):
     package     = "MG285"
     speed       = "8"
     default_clk = "clk"
@@ -126,6 +126,14 @@ class OrangeCrabR0_2Platform(LatticeECP5Platform):
         with products.extract("{}.bit".format(name)) as bitstream_filename:
             subprocess.check_call([dfu_util, "-D", bitstream_filename])
 
+class OrangeCrabR0_2FPlatform(_OrangeCrabR0_2Platform):
+    device      = "LFE5U-25F"
+
+class OrangeCrabR0_2_25FPlatform(_OrangeCrabR0_2Platform):
+    device      = "LFE5U-25F"
+
+class OrangeCrabR0_2_85FPlatform(_OrangeCrabR0_2Platform):
+    device      = "LFE5U-85F"
 
 if __name__ == "__main__":
     from .test.blinky import *

shrine-maiden-heavy-industries/torii-boards

Co-Authored-By: Daniel Maslowski <info@orangecms.org>
Co-Authored-By: Aki Van Ness <aki@lethalbit.net>

torii_boards/lattice/orangecrab_r0_2.py
@@ -14,8 +14,13 @@ from torii.platform.resources           import (
 	DirectUSBResource
 )
 
+# NOTE: Keep OrangeCrabR0_2FPlatform for backwards compatibility
+# Originally, there was only OrangeCrabR0_2FPlatform, but the 85F variant
+# needs a different device.
 __all__ = (
 	'OrangeCrabR0_2Platform',
+	'OrangeCrabR0_2_25FPlatform',
+	'OrangeCrabR0_2_85FPlatform',
 )
 
 
@@ -142,6 +147,11 @@ class OrangeCrabR0_2Platform(ECP5Platform):
 		with products.extract(f'{name}.bit') as bitstream_filename:
 			check_call([dfu_util, '-D', bitstream_filename])
 
+
+OrangeCrabR0_2_25FPlatform = OrangeCrabR0_2Platform
+
+class OrangeCrabR0_2_85FPlatform(OrangeCrabR0_2Platform):
+	device = 'LFE5U-85F'
 
 if __name__ == '__main__':
 	from ..test.blinky import Blinky