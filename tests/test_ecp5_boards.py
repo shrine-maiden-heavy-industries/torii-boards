c062930de66a5d487f8e6c02521ee9260aa84f3a
Aki Van Ness <aki@lethalbit.net>
2025-02-12 12:34:06 -0800

    tests: Cleaned up formatting

--- tests/test_ecp5_boards.py
+++ tests/test_ecp5_boards.py
@@ -1,17 +1,16 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
-from unittest                 import TestCase
 from pathlib                  import Path
+from unittest                 import TestCase
 
 from torii.platform.vendor    import LatticeECP5Platform
 
-from torii_boards.test.blinky import Blinky
 from torii_boards.lattice     import (
-	versa_ecp5, versa_ecp5_5g, orangecrab_r0_1, orangecrab_r0_2
+	orangecrab_r0_1, orangecrab_r0_2, versa_ecp5, versa_ecp5_5g
 )
+from torii_boards.test.blinky import Blinky
 
 BUILD_DIR = (Path(__file__).parent / '..' / 'build' / 'test_boards').resolve()
-
 
 class ToriiBoardsECP5Tests(TestCase):
 	def _test_platform(platform: LatticeECP5Platform):
@@ -22,7 +21,6 @@ class ToriiBoardsECP5Tests(TestCase):
 			plat_build = (BUILD_DIR / plat_name)
 			platform.build(Blinky(), plat_name, plat_build, do_build = True, do_program = False)
 		return test_platform
-
 
 	test_versa_ecp5        = _test_platform(versa_ecp5.VersaECP5Platform())
 	test_versa_ecp5_5g     = _test_platform(versa_ecp5_5g.VersaECP55GPlatform())
--- tests/test_ice40_boards.py
+++ tests/test_ice40_boards.py
@@ -1,17 +1,16 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
-from unittest                 import TestCase
 from pathlib                  import Path
+from unittest                 import TestCase
 
 from torii.platform.vendor    import LatticeICE40Platform
 
-from torii_boards.test.blinky import Blinky
 from torii_boards.lattice     import (
-	icebreaker, icebreaker_bitsy, ice40_up5k_b_evn, ice40_hx1k_blink_evn, ice40_hx8k_b_evn
+	ice40_hx1k_blink_evn, ice40_hx8k_b_evn, ice40_up5k_b_evn, icebreaker, icebreaker_bitsy
 )
+from torii_boards.test.blinky import Blinky
 
 BUILD_DIR = (Path(__file__).parent / '..' / 'build' / 'test_boards').resolve()
-
 
 class ToriiBoardsICE40Tests(TestCase):
 	def _test_platform(platform: LatticeICE40Platform):
@@ -22,7 +21,6 @@ class ToriiBoardsICE40Tests(TestCase):
 			plat_build = (BUILD_DIR / plat_name)
 			platform.build(Blinky(), plat_name, plat_build, do_build = True, do_program = False)
 		return test_platform
-
 
 	test_icebreaker           = _test_platform(icebreaker.ICEBreakerPlatform())
 	test_icebreaker_bitsy     = _test_platform(icebreaker_bitsy.ICEBreakerBitsyPlatform())
