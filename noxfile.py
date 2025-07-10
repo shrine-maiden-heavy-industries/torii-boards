2e4e03166f8736ef6c71da336934b33e2c4f34bc
Aki Van Ness <aki@lethalbit.net>
2025-07-10 07:51:55 -0700

    noxfile: Updated noxfile to match with Torii

--- noxfile.py
+++ noxfile.py
@@ -1,105 +1,134 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
-import shutil
+from os             import getenv
 from pathlib        import Path
 
 import nox
-from setuptools_scm import ScmVersion, get_version
+from nox.sessions   import Session
 
 ROOT_DIR  = Path(__file__).parent
 
-BUILD_DIR = (ROOT_DIR  / 'build')
-CNTRB_DIR = (ROOT_DIR  / 'contrib')
-DOCS_DIR  = (ROOT_DIR  / 'docs')
-DIST_DIR  = (BUILD_DIR / 'dist')
+BUILD_DIR = ROOT_DIR  / 'build'
+CNTRB_DIR = ROOT_DIR  / 'contrib'
+DOCS_DIR  = ROOT_DIR  / 'docs'
+DIST_DIR  = BUILD_DIR / 'dist'
+
+IN_CI           = getenv('GITHUB_WORKSPACE') is not None
+ENABLE_COVERAGE = IN_CI or getenv('TORII_TEST_COVERAGE') is not None
 
 # Default sessions to run
 nox.options.sessions = (
 	'test',
 	'lint',
-	'typecheck'
+	'typecheck-mypy'
 )
 
-def torii_boards_version() -> str:
-	def scheme(version: ScmVersion) -> str:
-		if version.tag and not version.distance:
-			return version.format_with('')
-		else:
-			return version.format_choice('+{node}', '+{node}.dirty')
-
-	return get_version(
-		root           = str(ROOT_DIR),
-		version_scheme = 'guess-next-dev',
-		local_scheme   = scheme,
-		relative_to    = __file__
-	)
 
 @nox.session(reuse_venv = True)
 def test(session: nox.Session) -> None:
+	OUTPUT_DIR = BUILD_DIR / 'tests'
+	OUTPUT_DIR.mkdir(parents = True, exist_ok = True)
+
+	unitest_args = ('-m', 'unittest', 'discover', '-s', str(ROOT_DIR))
+
 	session.install(
 		'git+https://github.com/shrine-maiden-heavy-industries/torii-hdl.git#egg=torii'
 	)
 	session.install('.')
-	session.run(
-		'python', '-m', 'unittest', 'discover',
-		'-s', '.'
-	)
+
+	# XXX(aki): Coverage is not quite possible yet
+	if False and ENABLE_COVERAGE:
+		session.log('Coverage support enabled')
+		session.install('coverage')
+		coverage_args = ('-m', 'coverage', 'run', '-p', f'--rcfile={ROOT_DIR / "pyproject.toml"}',)
+		session.env['COVERAGE_CORE'] = 'sysmon'
+	else:
+		coverage_args = tuple[str]()
+
+	with session.chdir(OUTPUT_DIR):
+		session.log('Running test suite...')
+		session.run('python', *coverage_args, *unitest_args, *session.posargs)
+
+	# XXX(aki): Coverage is not quite possible yet
+	if False and ENABLE_COVERAGE:
+		session.log('Combining Coverage data..')
+		session.run('python', '-m', 'coverage', 'combine')
+
+		session.log('Generating XML Coverage report...')
+		session.run('python', '-m', 'coverage', 'xml', f'--rcfile={ROOT_DIR / "pyproject.toml"}')
+
+@nox.session(name = 'watch-docs')
+def watch_docs(session: Session) -> None:
+	OUTPUT_DIR = BUILD_DIR / 'docs'
+
+	session.install('-r', str(DOCS_DIR / 'requirements.txt'))
+	session.install('sphinx-autobuild')
+	session.install('.')
+
+	session.run('sphinx-autobuild', str(DOCS_DIR), str(OUTPUT_DIR))
 
 @nox.session(name = 'build-docs')
-def build_docs(session: nox.Session) -> None:
-	out_dir = (BUILD_DIR / 'docs')
-	shutil.rmtree(out_dir, ignore_errors = True)
+def build_docs(session: Session) -> None:
+	OUTPUT_DIR = BUILD_DIR / 'docs'
+
 	session.install('-r', str(DOCS_DIR / 'requirements.txt'))
 	session.install(
 		'git+https://github.com/shrine-maiden-heavy-industries/torii-hdl.git#egg=torii'
 	)
 	session.install('.')
-	session.run('sphinx-build', '-b', 'html', str(DOCS_DIR), str(out_dir))
+
+	session.run('sphinx-build', '-b', 'html', str(DOCS_DIR), str(OUTPUT_DIR))
 
 @nox.session(name = 'linkcheck-docs')
-def linkcheck_docs(session: nox.Session) -> None:
-	out_dir = (BUILD_DIR / 'docs-linkcheck')
-	shutil.rmtree(out_dir, ignore_errors = True)
-	session.install('-r', str(DOCS_DIR / 'requirements.txt'))
-	session.install('.')
-	session.run('sphinx-build', '-b', 'linkcheck', str(DOCS_DIR), str(out_dir))
+def linkcheck_docs(session: Session) -> None:
+	OUTPUT_DIR = BUILD_DIR / 'docs-linkcheck'
 
-@nox.session
-def typecheck(session: nox.Session) -> None:
-	out_dir = (BUILD_DIR / 'mypy')
-	out_dir.mkdir(parents = True, exist_ok = True)
+	session.install('-r', str(DOCS_DIR / 'requirements.txt'))
+	session.install(
+		'git+https://github.com/shrine-maiden-heavy-industries/torii-hdl.git#egg=torii'
+	)
+	session.install('.')
+
+	session.run('sphinx-build', '-b', 'linkcheck', str(DOCS_DIR), str(OUTPUT_DIR))
+
+@nox.session(name = 'typecheck-mypy')
+def typecheck_mypy(session: Session) -> None:
+	OUTPUT_DIR = BUILD_DIR / 'typing' / 'mypy'
+	OUTPUT_DIR.mkdir(parents = True, exist_ok = True)
 
 	session.install('mypy')
 	session.install('lxml')
 	session.install('.')
+
 	session.run(
 		'mypy', '--non-interactive', '--install-types', '--pretty',
-		'--cache-dir', str((out_dir / '.mypy-cache').resolve()),
-		'--config-file', str((CNTRB_DIR / '.mypy.ini').resolve()),
-		'-p', 'torii_boards', '--html-report', str(out_dir.resolve())
+		'--disallow-any-generics',
+		'--cache-dir', str((OUTPUT_DIR / '.mypy-cache').resolve()),
+		'-p', 'torii_boards', '--html-report', str(OUTPUT_DIR.resolve())
 	)
+
+@nox.session(name = 'typecheck-pyright')
+def typecheck_pyright(session: Session) -> None:
+	OUTPUT_DIR = BUILD_DIR / 'typing' / 'pyright'
+	OUTPUT_DIR.mkdir(parents = True, exist_ok = True)
+
+	session.install('pyright')
+	session.install('.')
+
+	with (OUTPUT_DIR / 'pyright.log').open('w') as f:
+		session.run('pyright', *session.posargs, stdout = f)
 
 @nox.session
 def lint(session: nox.Session) -> None:
 	session.install('flake8')
+
 	session.run(
-		'flake8', '--config', str((CNTRB_DIR / '.flake8').resolve()), './torii_boards'
+		'flake8', '--config', str((CNTRB_DIR / '.flake8').resolve()),
+		'./torii_boards', './tests', './docs'
 	)
 
 @nox.session
-def dist(session: nox.Session) -> None:
+def dist(session: Session) -> None:
 	session.install('build')
-	session.run(
-		'python', '-m', 'build',
-		'-o', str(DIST_DIR)
-	)
 
-@nox.session
-def upload(session: nox.Session) -> None:
-	session.install('twine')
-	dist(session)
-	session.log(f'Uploading torii-boards-{torii_boards_version()} to PyPi')
-	session.run(
-		'python', '-m', 'twine',
-		'upload', f'{DIST_DIR}/torii*-{torii_boards_version()}*'
-	)
+	session.run('python', '-m', 'build', '-o', str(DIST_DIR))
