d356e5ddfeddb082d84a454159213186a84e622c
Aki Van Ness <aki@lethalbit.net>
2025-08-01 12:02:30 -0700

    torii_boards.lattice.machxo2: Lint fixes and formatting normalization

--- torii_boards/lattice/machxo2/tinyfpga_ax2.py
+++ torii_boards/lattice/machxo2/tinyfpga_ax2.py
@@ -16,7 +16,8 @@ class TinyFPGAAX2Platform(MachXO2Platform):
 	description = 'TinyFPGA AX2 Lattice MachXO2-1200 Development Board'
 
 	connectors  = [
-		Connector('gpio', 0,
+		Connector(
+			'gpio', 0,
 			# Left side of the board
 			#  1  2  3  4  5  6  7  8  9 10 11
 			'13 14 16 17 20 21 23 25 26 27 28 '
