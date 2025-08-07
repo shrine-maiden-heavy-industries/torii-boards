b3abb0f1371902bf53cab90bd691b87ec475b5ec
Aki Van Ness <aki@lethalbit.net>
2025-08-07 05:49:25 -0700

    torii_boards.xilinx.zynq7: Updated board resources and formatting

--- torii_boards/xilinx/zynq7/ebaz4205.py
+++ torii_boards/xilinx/zynq7/ebaz4205.py
@@ -1,9 +1,10 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
-from torii.build                  import Attrs, Clock, Pins, Resource
-from torii.build.run              import BuildProducts
-from torii.platform.resources     import LEDResources, UARTResource
-from torii.platform.vendor.xilinx import XilinxPlatform
+from torii.build                        import Attrs, Clock, Pins, Resource
+from torii.build.run                    import BuildProducts
+from torii.platform.resources.interface import UARTResource
+from torii.platform.resources.user      import LEDResources
+from torii.platform.vendor.xilinx       import XilinxPlatform
 
 __all__ = (
 	'EBAZ4205Platform',
