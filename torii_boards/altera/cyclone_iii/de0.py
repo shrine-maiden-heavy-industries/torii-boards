17ad7d598c94a724895b1d3313d41fd733f01f66
Aki Van Ness <aki@lethalbit.net>
2025-08-07 05:54:41 -0700

    torii_boards.altera.cyclone_iii: Updated board resources and formatting

--- torii_boards/altera/cyclone_iii/de0.py
+++ torii_boards/altera/cyclone_iii/de0.py
@@ -1,12 +1,12 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
-from torii.build                  import Attrs, Clock, Connector, Pins, Resource, Subsignal
-from torii.build.run              import BuildProducts
-from torii.platform.resources     import (
-	ButtonResources, Display7SegResource, LEDResources, NORFlashResources, PS2Resource, SDCardResources, SDRAMResource,
-	SwitchResources, UARTResource, VGAResource
-)
-from torii.platform.vendor.altera import AlteraPlatform
+from torii.build                        import Attrs, Clock, Connector, Pins, Resource, Subsignal
+from torii.build.run                    import BuildProducts
+from torii.platform.resources.display   import Display7SegResource, VGAResource
+from torii.platform.resources.interface import PS2Resource, UARTResource
+from torii.platform.resources.memory    import NORFlashResources, SDCardResources, SDRAMResource
+from torii.platform.resources.user      import ButtonResources, LEDResources, SwitchResources
+from torii.platform.vendor.altera       import AlteraPlatform
 
 __all__ = (
 	'DE0Platform',
