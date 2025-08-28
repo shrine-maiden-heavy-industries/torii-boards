0acbe2b1c455e8817137038bc8d71d1d5b6d5a2a
Aki Van Ness <aki@lethalbit.net>
2025-08-28 07:27:37 -0700

    noxfile: Added `build-docset` and `dist-docs` sessions
    This allows us to build and package documentation for offline distribution

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
-from os             import getenv
-from pathlib        import Path
-from shutil         import copy
+from os           import getenv
+from pathlib      import Path
+from shutil       import copy, make_archive, rmtree
 
 import nox
-from nox.sessions   import Session
+from nox.sessions import Session
 
 ROOT_DIR  = Path(__file__).parent
 
@@ -126,6 +126,48 @@ def build_docs_multiversion(session: Session) -> None:
 			session.warn(f'Docs for {latest} did not seem to be built, using development docs instead')
 			# Otherwise, link to `main`
 			latest_link.symlink_to(docs_dev)
+
+@nox.session(name = 'build-docset', reuse_venv = True)
+def build_docset(session: Session) -> None:
+	DOCS_DIR = BUILD_DIR / 'docs'
+
+	# XXX(aki): We can't `session.notify` here because we need the docs first
+	build_docs(session)
+
+	session.install('doc2dash')
+
+	# Get the Torii Boards version
+	torii_boards_version: str = session.run(
+		'python', '-c', 'import torii_boards;print(torii_boards.__version__)',
+		silent = True
+	)
+
+	with session.chdir(BUILD_DIR):
+		# If the docset is already built, shred it because `doc2dash` won't overwrite it
+		if (BUILD_DIR / 'Torii_Boards.docset').exists():
+			rmtree(BUILD_DIR / 'Torii_Boards.docset')
+
+		# Build the docset
+		session.run(
+			'doc2dash', '-n', 'Torii_Boards', '-j', '--full-text-search', 'on', str(DOCS_DIR)
+		)
+
+		# Compress it
+		make_archive(f'torii_boards-{torii_boards_version.strip()}-docset', 'zip', BUILD_DIR, 'Torii_Boards.docset')
+
+@nox.session(name = 'dist-docs', reuse_venv = True)
+def dist_docs(session: Session) -> None:
+	# XXX(aki): We can't `session.notify` here because we need the docs first
+	build_docs(session)
+
+	# Get the Torii Boards version
+	torii_boards_version: str = session.run(
+		'python', '-c', 'import torii_boards;print(torii_boards.__version__)',
+		silent = True
+	)
+
+	with session.chdir(BUILD_DIR):
+		make_archive(f'torii_boards-{torii_boards_version.strip()}-docs', 'zip', BUILD_DIR, 'docs')
 
 @nox.session(name = 'linkcheck-docs', reuse_venv = True)
 def linkcheck_docs(session: Session) -> None:
