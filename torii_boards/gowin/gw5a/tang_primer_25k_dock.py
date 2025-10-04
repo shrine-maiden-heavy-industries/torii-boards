6d1e417542adba01a1c6d8aa48da219464f44345
Aria Burrell <litui@litui.ca>
2025-10-04 17:33:52 -0600

    Switched model and commands to support Apicula and Yosys toolchain stuff.
    - Added comments

--- torii_boards/gowin/gw5a/tang_primer_25k_dock.py
+++ torii_boards/gowin/gw5a/tang_primer_25k_dock.py
@@ -1,14 +1,16 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
 import subprocess
-from enum import Enum, auto
-from torii.build import Attrs, Clock, Pins, Resource, Connector
-from torii.platform.resources.interface import UARTResource, DirectUSBResource
+from enum import Enum
+
+from torii.build import Attrs, Clock, Connector, Pins, Resource
+from torii.platform.resources.interface import DirectUSBResource, UARTResource
 from torii.platform.resources.memory import SPIFlashResources
 from torii.platform.resources.user import ButtonResources, LEDResources
 from torii.platform.vendor.gowin import GowinPlatform
 
 __all__ = ("TangPrimer25kDockPlatform",)
+
 
 class GowinProgrammerOp(Enum):
 	"""
@@ -28,62 +30,61 @@ class TangPrimer25kDockPlatform(GowinPlatform):
 	* https://github.com/YosysHQ/apicula/blob/master/examples/gw5a/primer25k.cst
 	"""
 
-	part = "GW5A-LV25MG121NC1/I0"
+	# Using the 'ES' (Engineering Sample) version of the board as the
+	# C1/I0 variant is not working in Apicula at this time.
+	part = "GW5A-LV25MG121NES"
 	family = "GW5A-25A"
-	default_clk = "clk50m"
-	osc_frequency = 50e6
+
+	# This is the 50MHz crystal on the Primer 25K Dock.
+	# The builtin oscillator may also be used by specifying "OSC"
+	# along with a frequency in line with allowed clock divisions:
+	# "3 or an even number between 2 and 126."
+	default_clk = "clk50"
+	osc_frequency = 50_000_000
 
 	pretty_name = "Tang Primer 25K Dock"
 	description = "Sipeed Tang Primer 25K Dock Gowin GW5A Development Board"
 
 	resources = [
-		# External crystal @ 50MHz on pin E2
 		Resource(
-			"clk50m",
+			"clk50",
 			0,
 			Pins("E2", dir="i"),
-			Clock(50e6),
-			Attrs(IO_TYPE="LVCMOS33", PULL_MODE="NONE", DRIVE="OFF",
-				  BANK_VCCIO="3.3"),
+			Clock(50_000_000),
+			Attrs(IO_TYPE="LVCMOS33"),
 		),
 		*ButtonResources("button", 0, pins="H10 H11 F5 G7 H7 J5"),
 		*LEDResources(
 			"led",
-			pins="G5", # G8 H7 J5 F5 G7 H8 H5",
-			attrs=Attrs(IO_TYPE="LVCMOS33", PULL_MODE="NONE"),
+			pins="G5 G8 H7 J5 F5 G7 H8 H5",
+			attrs=Attrs(IO_TYPE="LVCMOS33"),
 		),
 		DirectUSBResource(
 			"usb",
 			0,
 			d_p="L6",
 			d_n="K6",
-			attrs=Attrs(
-				IO_TYPE="LVCMOS33", PULL_MODE="DOWN", DRIVE=8, BANK_VCCIO="3.3"
-			),
+			attrs=Attrs(IO_TYPE="LVCMOS33"),
 		),
 		UARTResource(0, tx="C3", rx="B3", attrs=Attrs(IO_TYPE="LVCMOS33")),
-		# *SPIFlashResources(
-		# 	0,
-		# 	cs_n="B1",
-		# 	clk="C1",
-		# 	copi="D6",
-		# 	cipo="E5",
-		# 	wp_n="E2",
-		# 	hold_n="D1",
-		# 	attrs=Attrs(IO_TYPE="LVCMOS33"),
-		# ),
+		# These flash pins /should/ be correct
+		# Sipeed did not do a good job of documenting these
+		*SPIFlashResources(
+			0,
+			cs_n="B1",
+			clk="C1",
+			copi="D6",
+			cipo="E5",
+			wp_n="E2",
+			hold_n="D1",
+			attrs=Attrs(IO_TYPE="LVCMOS33"),
+		),
 	]
 
 	connectors = [
-		Connector(
-			"pmod", 0, "G11 D11 B11 C11 -   -   G10 D10 B10 C10 -   -   "
-		),  # PMOD1A
-		Connector(
-			"pmod", 1, "A11 E11 K11 L5  -   -   A10 E10 L11 K5  -   -   "
-		),  # PMOD1B
-		Connector(
-			"pmod", 2, "F5  G7  H8  H5  -   -   G5  G8  H7  J5  -   -   "
-		),  # PMOD2
+		Connector("pmod", 0, "G11 D11 B11 C11 -   -   G10 D10 B10 C10 -   -   "),  # PMOD1A
+		Connector("pmod", 1, "A11 E11 K11 L5  -   -   A10 E10 L11 K5  -   -   "),  # PMOD1B
+		Connector("pmod", 2, "F5  G7  H8  H5  -   -   G5  G8  H7  J5  -   -   "),  # PMOD2
 		Connector(
 			"gpio",
 			0,
@@ -94,47 +95,49 @@ class TangPrimer25kDockPlatform(GowinPlatform):
 		),
 	]
 
-	def __init__(self, *args, toolchain='Gowin', use_gowin_programmer=True,
-			gowin_programmer_op=GowinProgrammerOp.SRAMProgram):
-		"""
-		Tang Primer 25k Dock Platform
-
-		Supports specification of toolchain (only 'Gowin' supported at
-		this time) and use_gowin_programmer=True enables use of the
-		vendor-supplied programmer_cli tool which can erase and write to
-		the embedded flash if specified as the gowin_programmer_op
-		operation.
-
-		Warning: writing to exFlash is quite slow via this method.
-		
-		The gowin programmer options are ignored if 'Apicula'
-		toolchain is selected.
-		"""
+	def __init__(
+		self,
+		*args,
+		toolchain="Apicula",
+		use_gowin_programmer=True,
+		gowin_programmer_op=GowinProgrammerOp.SRAMProgram,
+		**kwargs,
+	):
 		self.use_gowin_programmer = use_gowin_programmer
 		self.gowin_programmer_op = gowin_programmer_op
 
-		super().__init__(*args, toolchain=toolchain)
+		super().__init__(*args, toolchain=toolchain, nextpnr_tool_variant="himbaechel", **kwargs)
 
 	def toolchain_prepare(self, fragment, name, **kwargs):
 		overrides = {
-			"add_options": "set_option -use_sspi_as_gpio 1 -use_cpu_as_gpio 1",
-			"gowin_pack_opts": "--sspi_as_gpio --cpu_as_gpio",
+			"add_options": "set_option -use_mspi_as_gpio 1 -use_sspi_as_gpio 1 -use_cpu_as_gpio 1",
+			"nextpnr_opts": "--vopt mspi_as_gpio --vopt sspi_as_gpio --vopt cpu_as_gpio",
+			"gowin_pack_opts": "--mspi_as_gpio --sspi_as_gpio --cpu_as_gpio",
 		}
 		return super().toolchain_prepare(fragment, name, **overrides, **kwargs)
 
-	def toolchain_program(self, products, name):
+	def toolchain_program(self, products, name, **kwargs):
 		with products.extract(f"{name}.fs") as bitstream_filename:
-			if self.use_gowin_programmer:
+			# Use the Gowin programmer if the toolchain is installed.
+			# The programmer operation can be specified as well to allow
+			# writing to flash (not supported with openFPGAloader).
+			if self.toolchain == "Gowin" and self.use_gowin_programmer:
 				subprocess.check_call(
-					["programmer_cli", "--device", self.family, 
-	  				"--frequency", "15MHz", "--operation_index",
-	  				f"{self.gowin_programmer_op.value}", "--fsFile", bitstream_filename]
+					[
+						"programmer_cli",
+						"--device",
+						self.family,
+						"--frequency",
+						"15MHz",
+						"--operation_index",
+						f"{self.gowin_programmer_op.value}",
+						"--fsFile",
+						bitstream_filename,
+					]
 				)
 			else:
 				# Loads only to SRAM at present, so does not persist upon reset
-				subprocess.check_call(
-					["openFPGALoader", "-b", "tangprimer25k", bitstream_filename]
-				)
+				subprocess.check_call(["openFPGALoader", "-b", "tangprimer25k", bitstream_filename])
 
 
 if __name__ == "__main__":
