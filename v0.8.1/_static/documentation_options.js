f2494357dc7419b7a4f9fa0e23fe16a6b93744a6
lethalbit <201479+lethalbit@users.noreply.github.com>
2026-02-22 05:16:06 +0000

    Deploying to gh-pages from @ shrine-maiden-heavy-industries/torii-boards@be5622b9e3ff9747af4a76714b09285ad5fc23a6 🚀

---
+++ v0.8.1/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.8.2.dev30',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: false,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
